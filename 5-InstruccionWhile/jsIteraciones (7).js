cdc4b9996bc819916ba286e5b957480118cea05a
florencia-sys <52495075+florencia-sys@users.noreply.github.com>
2019-07-16 02:05:40 +0200

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,20 @@
 function mostrar()
 {
-
+	var promedio;
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+    var numero;
 
+while (contador= 'si'){
+
+numero = parseInt(prompt ("Ingrese un numero"));
+acumulador = acumulador + numero;
+
+contador ++;
+}
+promedio = acumulador/ contador;
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
-
 }//FIN DE LA FUNCIÓN
