388c6da8b8185d29ad576f388b31edfd08ff4311
florencia-sys <52495075+florencia-sys@users.noreply.github.com>
2019-07-17 18:41:50 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,11 +3,12 @@ function mostrar()
 	var promedio;
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta='s';
     var numero= 0;
 
-while (respuesta== 'si'){
+while (respuesta =='s'){
 
+prompt ("Quiere seguir ingresando numeros ? s/n")
 numero = parseInt(prompt ("Ingrese un numero"));
 acumulador = acumulador + numero;
 
